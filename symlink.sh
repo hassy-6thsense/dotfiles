eeb5082bf42feed995405265e59f5d386dcf83cc
hassy <hassy.6thsense@gmail.com>
2014-04-14 15:49:24 +0900

    ファイル名の変数をクォートで囲んだ

--- symlink.sh
+++ symlink.sh
@@ -2,11 +2,11 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ ${dotfile} != "." ] && [ ${dotfile} != ".." ] && [ ${dotfile} != ".git" ] && [ ${dotfile} != ".gitignore" ]; then
-		if [ -e ${HOME}/${dotfile} ]; then
+    if [ "${dotfile}" != "." ] && [ "${dotfile}" != ".." ] && [ "${dotfile}" != ".git" ] && [ "${dotfile}" != ".gitignore" ]; then
+		if [ -e "${HOME}/${dotfile}" ]; then
 			rm -ir "${HOME}/${dotfile}"
 		fi
-        ln -vFis "${PWD}/${dotfile}" ${HOME}
+        ln -vFis "${PWD}/${dotfile}" "${HOME}"
     fi
 done
 
