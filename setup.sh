77c260fd5fd1e9ac439caadf86d412d13a1fd8d2
hassy <hassy.6thsense@gmail.com>
2014-12-05 18:17:11 +0900

    Refactored setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ git submodule update
 
 # Make symbolic links to /path/to/dotfiles/* in $HOME. 
 cd "$(dirname $0)"
-echo $PWD
 for dotfile in .?*
 do
     if [ "${dotfile}" != "." ] &&
@@ -17,29 +16,30 @@ do
 		if [ -e "${HOME}/${dotfile}" ]; then
 			rm -ir "${HOME}/${dotfile}"
 		fi
-        ln -vFis "${PWD}/${dotfile}" "${HOME}"
+        ln -vFis "${PWD}/${dotfile}" "${HOME}/${dotfile}"
     fi
 done
 
 # Make symbolic links to $HOME/.zsh/.om-my-zsh/custom/* in /path/to/oh-my-zsh/custom/* 
+omz_custom_dir="${HOME}/.oh-my-zsh/custom"
+omz_custom_plugins_dir="${HOME}/.oh-my-zsh/custom/plugins"
+
 cd "${PWD}/.zsh/.oh-my-zsh/custom"
-echo $PWD
 for file in ?*
 do
     if [ "${file}" != "plugins" ]; then
-		if [ -e "${HOME}/.oh-my-zsh/custom/${file}" ]; then
-			rm -ir "${HOME}/.oh-my-zsh/custom/${file}"
+		if [ -e "${omz_custom_dir}/${file}" ]; then
+			rm -ir "${omz_custom_dir}/${file}"
 		fi
-        ln -vFis "${PWD}/${file}" "${HOME}/.oh-my-zsh/custom/${file}"
+        ln -vFis "${PWD}/${file}" "${omz_custom_dir}/${file}"
 	else 
 		cd "${PWD}/plugins"
-		echo $PWD
 		for plugin in ?*
 		do
-			if [ -e "${HOME}/.oh-my-zsh/custom/plugins/${plugin}" ]; then
-				rm -ir "${HOME}/.oh-my-zsh/custom/plugins/${plugin}"
+			if [ -e "${omz_custom_plugins_dir}/${plugin}" ]; then
+				rm -ir "${omz_custom_plugins_dir}/${plugin}"
 			fi
-        	ln -vFis "${PWD}/${plugin}" "${HOME}/.oh-my-zsh/custom/plugins/${plugin}"
+        	ln -vFis "${PWD}/${plugin}" "${omz_custom_plugins_dir}/${plugin}"
 		done
 	fi
 done
