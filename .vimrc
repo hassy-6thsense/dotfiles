83257d40399dc8adc385820a346f4d4b63553348
hassy <hassy.6thsense@gmail.com>
2014-12-19 18:33:22 +0900

    Changed to 4-whitespace from tab

--- .vimrc
+++ .vimrc
@@ -1,17 +1,17 @@
 "----------基本設定----------
 if filereadable( expand( '~/.vim/basic.vimrc' ) )
-	source ~/.vim/basic.vimrc
+    source ~/.vim/basic.vimrc
 endif
 
 "----------色設定----------
 if filereadable( expand( '~/.vim/color.vimrc' ) )
-	source ~/.vim/color.vimrc
+    source ~/.vim/color.vimrc
 endif
 
 "----------neobundle----------
 if filereadable( expand( '~/.vim/neobundle.vimrc' ) ) && filereadable( expand( '~/.vim/bundle/neobundle.vim/README.md' ) )
-	source ~/.vim/neobundle.vimrc
+    source ~/.vim/neobundle.vimrc
 else
-	echo "Type 'git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim'."
+    echo "Type 'git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim'."
 endif
 
--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 function divert_file() {
-	if [ -L "${1}" ]; then
-		rm -f "${1}"
-	elif [ -e "${1}" ]; then
-		echo -n "mv: "
-		mv -vi "${1}" "${1}.orig"
-	fi
+    if [ -L "${1}" ]; then
+        rm -f "${1}"
+    elif [ -e "${1}" ]; then
+        echo -n "mv: "
+        mv -vi "${1}" "${1}.orig"
+    fi
 }
 
 function make_symlink() {
-	divert_file "${2}"
-	echo -n "ln: "
-	ln -vis "${1}" "${2}"
+    divert_file "${2}"
+    echo -n "ln: "
+    ln -vis "${1}" "${2}"
 }
 
 dotfiles_dir="${HOME}/dotfiles"
@@ -25,21 +25,21 @@ fi
 git submodule init
 git submodule update
 
-# Make symbolic links to /path/to/dotfiles/* in $HOME. 
+# Make symbolic links to /path/to/dotfiles/* in $HOME.
 cd "${dotfiles_dir}"
 for dotfile in .?*
 do
     if [ "${dotfile}" != "." ] &&
-	   [ "${dotfile}" != ".." ] &&
-	   [ "${dotfile}" != ".git" ] &&
-	   [ "${dotfile}" != ".gitfiles" ] &&
-	   [ "${dotfile}" != ".gitignore" ] &&
-	   [ "${dotfile}" != ".gitmodules" ]; then
+       [ "${dotfile}" != ".." ] &&
+       [ "${dotfile}" != ".git" ] &&
+       [ "${dotfile}" != ".gitfiles" ] &&
+       [ "${dotfile}" != ".gitignore" ] &&
+       [ "${dotfile}" != ".gitmodules" ]; then
         make_symlink "${PWD}/${dotfile}" "${HOME}/${dotfile}"
     fi
 done
 
-# Make symbolic links to /path/to/dotfiles/gitfiles/* in $HOME. 
+# Make symbolic links to /path/to/dotfiles/gitfiles/* in $HOME.
 cd "${dotfiles_dir}/.gitfiles"
 for dotfile in .?*
 do
@@ -57,12 +57,12 @@ for file in ?*
 do
     if [ "${file}" != "plugins" ]; then
         make_symlink "${PWD}/${file}" "${omz_custom_dir}/${file}"
-	else 
-		cd "${PWD}/plugins"
-		for plugin in ?*
-		do
-        	make_symlink "${PWD}/${plugin}" "${omz_custom_plugins_dir}/${plugin}"
-		done
-	fi
+    else
+        cd "${PWD}/plugins"
+        for plugin in ?*
+        do
+            make_symlink "${PWD}/${plugin}" "${omz_custom_plugins_dir}/${plugin}"
+        done
+    fi
 done
 
